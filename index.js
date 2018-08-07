50f4bf389f30194c13a5fa70ffe31efd11a4c215
David Chau <thechausenone7@gmail.com>
2018-08-07 19:39:51 -0400

    feat(repo): setup express app

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
